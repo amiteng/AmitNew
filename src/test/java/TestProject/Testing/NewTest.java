b4232e06bd8df4b4cbb77f4620d9238ff88c628c
AmitKumar <amit.ku@corp.endurance.com>
2018-11-09 12:15:51 +0530

    Testing Project Is Going To Be Added In New Repo

---
+++ src/test/java/TestProject/Testing/NewTest.java
@@ -0,0 +1,30 @@
+package TestProject.Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+
+	public WebDriver driver;
+	
+	  @Test
+	  public void openMyBlogagain() {
+		driver.get("https://www.softwaretestingmaterial.com/");
+		System.out.println("this Is Amit");
+	  }                                         
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+		  System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\driver\\geckodriver.exe");
+		  driver = new FirefoxDriver();
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit();
+	  }
+
+	}
